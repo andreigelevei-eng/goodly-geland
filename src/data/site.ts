e934c52934bb92eecc1c9e08668e1fe0430270ed
andreigelevei-eng <andrei.gelevei@gmail.com>
2026-05-30 19:15:57 +0300

    Add site settings

---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+export const site = {
+  name: 'Обзорник',
+  description: 'Обзоры обучающих материалов, курсов и практических руководств.',
+  url: 'https://obzornik.online',
+  goodlyStoreUrl: 'https://geland.goodly.pro/store/1c2hmudh',
+  defaultCtaText: 'Перейти в магазин Geland',
+};
