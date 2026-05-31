abef53477c5984ab13fc3028ad9e9a1f787d3f3c
andreigelevei-eng <andrei.gelevei@gmail.com>
2026-06-01 00:49:17 +0300

    Set site metadata to Goodly subdomain

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const site = {
-  name: 'Обзорник',
-  description: 'Обзоры обучающих материалов, курсов и практических руководств.',
-  url: 'https://obzornik.online',
+  name: 'Обзорник / Goodly',
+  description: 'Обзоры обучающих материалов Goodly: содержание, польза, кому подойдёт и ссылка на продукт.',
+  url: 'https://goodly.obzornik.online',
   goodlyStoreUrl: 'https://geland.goodly.pro/store/1c2hmudh',
   defaultCtaText: 'Перейти в магазин Geland',
 };
